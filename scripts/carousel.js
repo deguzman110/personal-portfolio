830863e0d4c7cd72a31ff40a174752c5bbe69781
Anthony DeGuzman <deguzman110@gmail.com>
2018-05-30 13:33:06 -0400

    Fixed carousel.js, project1.html, and project1.css

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,61 +2,29 @@
 // Next = counter +1
 // Prev = counter -1
 
-// My Code
-var currentSlideindex = 1;
-var slidesArray = Array.from(document.getElementsByClassName("project-slide"));
+var currentSlideIndex = 1;
+var slidesArray = Array.from(document.getElementsByClassName('project-slide'));
 
-var nextSlide = document.querySelector(".next");
-var prevSlide = document.querySelector(".prev");
+var next = document.querySelector('.next');
+var prev = document.querySelector('.prev');
 
-function changeSlide(event) {
-  slidesArray[currentSlideIndex].classList.remove("active");
+function changeSlide(event){
+  console.log(event.target.className)
 
-  if(event.target.className.includes("prev")) {
+  slidesArray[currentSlideIndex].classList.remove('active');
+
+  if(event.target.className.includes('prev')){
     currentSlideIndex--;
-    if(currentSlideIndex < 0) {
-      currentSlideIndex = slidesArray.length - 1;
-    }
+    if(currentSlideIndex < 0){ currentSlideIndex = slidesArray.length - 1; }
   }
 
-  else if(event.target.className.includes("next")) {
+  else if(event.target.className.includes('next')){
     currentSlideIndex++;
-    if(currentSlideIndex >= slidesArray.length) {
-      currentSlideIndex = 0;
-    }
+     if(currentSlideIndex >= slidesArray.length){ currentSlideIndex = 0; }
   }
-
-  slidesArray[currentSlideIndex].className += " active";
+  slidesArray[currentSlideIndex].className += ' active';
+  console.log(currentSlideIndex)
 }
 
-  nextSlide.addEventListener("click", changeSlide);
-  prevSlide.addEventListener("click", changeSlide);
-
-// Alissa's Code
-//
-// var currentSlideIndex = 1;
-// var slidesArray = Array.from(document.getElementsByClassName('project-slide'));
-//
-// var next = document.querySelector('.next');
-// var prev = document.querySelector('.prev');
-//
-// function changeSlide(event){
-//   console.log(event.target.className)
-//
-//   slidesArray[currentSlideIndex].classList.remove('active');
-//
-//   if(event.target.className.includes('prev')){
-//     currentSlideIndex--;
-//     if(currentSlideIndex < 0){ currentSlideIndex = slidesArray.length - 1; }
-//   }
-//
-//   else if(event.target.className.includes('next')){
-//     currentSlideIndex++;
-//      if(currentSlideIndex >= slidesArray.length){ currentSlideIndex = 0; }
-//   }
-//   slidesArray[currentSlideIndex].className += ' active';
-//   console.log(currentSlideIndex)
-// }
-//
-// next.addEventListener('click', changeSlide);
-// prev.addEventListener('click', changeSlide);
+next.addEventListener('click', changeSlide);
+prev.addEventListener('click', changeSlide);
